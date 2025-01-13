13ceb26e0824a8affe13bc1ff50f8652a0ee43f0
Marko Golovko <markgolovko@gmail.com>
2025-01-13 09:36:57 +0100

    update metadata

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,12 +1,12 @@
 export default {
-	title: "Рекорди України зі стрільби з лука",
-	url: "https://example.com/",
+	title: "Рекорди України зі стрільби з блочного лука",
+	url: "https://compound-records.com/",
 	language: "ua",
-	description: "Дізнайтеся про захоплюючі досягнення та віхи українських лучників",
+	description: "Рекорди харківській області, України та світу зі стрільби з блочного лука у приміщенні та просто неба серед чоловіків, жінок та змішанних команд, кадетів, юніорів та майстрів",
 	ico: "/favicon.ico",
 	author: {
 		name: "Marko Golovko",
 		email: "markogolovko@proton.me",
-		url: "https://example.com/about-me/"
+		url: "https://markogolovko.com/"
 	}
 }
